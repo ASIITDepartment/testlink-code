e6fc75d7c0a253f7fba6745776a52954e96fcb59
root <bradybod@gmail.com>
2019-09-27 08:41:16 +0000

    THIS IS WORKING AND UPDATED

--- config_db.inc.php
+++ config_db.inc.php
@@ -3,4 +3,4 @@ define('DB_TYPE', 'mysql');
 define('DB_USER', 'root');
 define('DB_PASS', 'MR7VFfs9troB');
 define('DB_HOST', 'localhost:3306');
-define('DB_NAME', 'testlink');
+define('DB_NAME', 'asi_testlink');
