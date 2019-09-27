86491bd718920b285d0f41dac24487fcc6d6ea31
root <bradybod@gmail.com>
2019-09-27 08:25:05 +0000

    Added Correct DB

--- config_db.inc.php
+++ config_db.inc.php
@@ -1,6 +1,6 @@
 <?php
 define('DB_TYPE', 'mysql');
-define('DB_USER', 'bn_testlink');
-define('DB_PASS', '0931cb2028');
+define('DB_USER', 'root');
+define('DB_PASS', 'MR7VFfs9troB');
 define('DB_HOST', 'localhost:3306');
-define('DB_NAME', 'bitnami_testlink');
+define('DB_NAME', 'testlink');
