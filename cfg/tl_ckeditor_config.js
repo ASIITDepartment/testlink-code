926ba77b81f905121cac013ee1ddaba44c98575c
asiitdepartment <itdepartment@aisrobots.com>
2019-11-22 22:41:02 +0000

    Updated ckeditor

--- cfg/tl_ckeditor_config.js
+++ cfg/tl_ckeditor_config.js
@@ -13,70 +13,93 @@ CKEDITOR.editorConfig = function( config )
 	// choose your prefered ckedtior skin
 	// available skins for version 4.x: moono-lisa => default
 	// For skins present on version 3.x => http://ckeditor.com/addons/skins/all
-	config.skin = 'moonocolor';
+	config.skin = 'moono';
 	
 	// set css of ckeditor content to testlink.css
 	config.contentsCss = 'gui/themes/default/css/testlink.css';
 	
 	// do not check "Replace actual contents" checkbox as default
 	config.templates_replaceContent = false;
-	
-	// default Toolbar
-	config.toolbar_Testlink = 
-	[
-		['Source','Templates','SpellChecker','Find','Undo','Redo','-',
-		 'NumberedList','BulletedList','-',
-		 'JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock','-',
-		 'Outdent','Indent','-',
-		 'Table','HorizontalRule',],
-		 '/',
-		 ['Format','Bold','Italic','Underline','Strike','-',
-		  'Subscript','Superscript','-','TextColor','BGColor','RemoveFormat','-',
-		  'Link','Image','Anchor','SpecialChar']
-	];
-	
-	// mini Toolbar
-	config.toolbar_TestlinkMini = 
-	[
-		['NumberedList','BulletedList','-',
-		 'JustifyLeft','JustifyCenter','JustifyRight','-',
-		 'Bold','Italic','TextColor','-',
-		 'Link','Image','Table']
-	];
-	
+
+	config.toolbarCanCollapse = true;
+	config.toolbarStartupExpanded = false;
+	config.undoStackSize = 100;
+
+	config.width = '100%';
+
 	// Toolbar with all available features - can be used as template for custom toolbars
 	// '-' creates toolbar seperator
 	// '/' creates a new toolbar "line"
 	// [...] defines sub-toolbars
-	config.toolbar_Full =
-	[
-	 	['Source','-','Save','NewPage','Preview','-','Templates'],
-	    ['Cut','Copy','Paste','PasteText','PasteFromWord','-','Print', 'SpellChecker', 'Scayt'],
-	    ['Undo','Redo','-','Find','Replace','-','SelectAll','RemoveFormat'],
-	    ['Form', 'Checkbox', 'Radio', 'TextField', 'Textarea', 'Select', 'Button', 'ImageButton', 'HiddenField'],
-	    '/',
-	    ['Bold','Italic','Underline','Strike','-','Subscript','Superscript'],
-	    ['NumberedList','BulletedList','-','Outdent','Indent','Blockquote','CreateDiv'],
-	    ['JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock'],
-	    ['BidiLtr', 'BidiRtl' ],
-	    ['Link','Unlink','Anchor'],
-	    ['Image','Flash','Table','HorizontalRule','Smiley','SpecialChar','PageBreak'],
-	    '/',
-	    ['Styles','Format','Font','FontSize'],
-	    ['TextColor','BGColor'],
-	    ['Maximize','ShowBlocks','-','About']
+	// config.toolbar_Full =
+	// [
+	//  	['Source','-','Save','NewPage','Preview','-','Templates'],
+	//     ['Cut','Copy','Paste','PasteText','PasteFromWord','-','Print', 'SpellChecker', 'Scayt'],
+	//     ['Undo','Redo','-','Find','Replace','-','SelectAll','RemoveFormat'],
+	//     ['Form', 'Checkbox', 'Radio', 'TextField', 'Textarea', 'Select', 'Button', 'ImageButton', 'HiddenField'],
+	//     '/',
+	//     ['Bold','Italic','Underline','Strike','-','Subscript','Superscript'],
+	//     ['NumberedList','BulletedList','-','Outdent','Indent','Blockquote','CreateDiv'],
+	//     ['JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock'],
+	//     ['BidiLtr', 'BidiRtl' ],
+	//     // ['Link','Unlink','Anchor'],
+	//     ['EqnEditor','CodeSnippet','Image','Link','Flash','Table','HorizontalRule','Smiley','SpecialChar','filemanager'],
+	//     '/',
+	//     ['Styles','Format','Font','FontSize'],
+	//     ['TextColor','BGColor'],
+	//     ['Maximize','ShowBlocks','-','About']
+	// ];
+
+	// toolLayout = [
+	// 	['Maximize','-'],
+	// 	['Undo','Redo','-','Find','Replace','-'],
+	// 	['EqnEditor','CodeSnippet','Image','Link','Table','SpecialChar','HorizontalRule'],
+	// 	['Outdent','Indent','-','Blockquote',],
+	// 	'/',
+	// 	['Bold','Italic','Underline','Strike','Subscript','Superscript','-'],
+	// 	['NumberedList','BulletedList','-'],
+	// 	['JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock','-'],
+	// 	['TextColor','BGColor','RemoveFormat'],
+	// 	// '/',
+	// 	// ['Styles','Format','Font','FontSize'],
+	// ];
+
+	toolLayout = [
+		{ name: 'window', items: ['Maximize'] },
+		{ name: 'clipboard', items: [ 'Undo', 'Redo', '-', 'Find', 'Replace'] },
+		{ name: 'insert', items: ['EqnEditor','CodeSnippet','Image','Link','Table','SpecialChar','HorizontalRule'] },
+		{ name: 'paragraphFormat', items: ['Outdent','Indent','-','Blockquote',] },
+		'/',
+		{ name: 'basicstyles', items: ['Bold','Italic','Underline','Strike','Subscript','Superscript'] },
+		{ name: 'lists', items: ['NumberedList','BulletedList','-'] },
+		{ name: 'lineFormat', items: ['JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock'] },
+		{ name: 'textColor', items: ['TextColor','BGColor','RemoveFormat'] }
 	];
+	
+	// default Toolbar
+	config.toolbar_Testlink = toolLayout;
+	
+	// mini Toolbar
+	config.toolbar_TestlinkMini = toolLayout;
+	
+	
+	// full Toolbar
+	config.toolbar_Full = toolLayout;
 	
 	/* Configuration of File Browser
 	   You can use theses definitions if you buy ckfinder
 	   more informations on http://ckfinder.com/
 	   download ckfinder and put into third party folder
 	*/
-	//config.filebrowserBrowseUrl = '/third_party/ckfinder/ckfinder.html';
-	//config.filebrowserImageBrowseUrl = '/third_party/ckfinder/ckfinder.html?Type=Images';
-	//config.filebrowserFlashBrowseUrl = '/third_party/ckfinder/ckfinder.html?Type=Flash';
-	// uncomment these lines only if you want to allow quick upload
-	//config.filebrowserUploadUrl = '/third_party/ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Files';
-	//config.filebrowserImageUploadUrl = '/third_party/ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Images';
-	//config.filebrowserFlashUploadUrl = '/third_party/ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Flash';
+	config.filebrowserBrowseUrl = '/third_party/ckeditor/plugins/filemanager/dialog.php?type=2&editor=ckeditor&fldr=';
+	config.filebrowserImageBrowseUrl = '/third_party/ckeditor/plugins/filemanager/dialog.php?type=1&editor=ckeditor&fldr=';
+	config.filebrowserUploadUrl = '/third_party/ckeditor/plugins/filemanager/dialog.php?type=2&editor=ckeditor&fldr=';
+
+	// config.filebrowserBrowseUrl = '/third_party/ckfinder/ckfinder.html';
+	// config.filebrowserImageBrowseUrl = '/third_party/ckfinder/ckfinder.html?Type=Images';
+	// // config.filebrowserFlashBrowseUrl = '/third_party/ckfinder/ckfinder.html?Type=Flash';
+	// // uncomment these lines only if you want to allow quick upload
+	// config.filebrowserUploadUrl = '/third_party/ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Files';
+	// config.filebrowserImageUploadUrl = '/third_party/ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Images';
+	// // config.filebrowserFlashUploadUrl = '/third_party/ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Flash';
 }
--- config_db.inc.php
+++ config_db.inc.php
@@ -1,6 +1,6 @@
 <?php
 define('DB_TYPE', 'mysql');
 define('DB_USER', 'root');
-define('DB_PASS', 'MR7VFfs9troB');
+define('DB_PASS', 'OjOHdcnN1FhX');
 define('DB_HOST', 'localhost:3306');
-define('DB_NAME', 'asi_testlink');
+define('DB_NAME', 'bitnami_testlink');
