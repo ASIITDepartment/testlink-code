c109fba8055de1d898749cb20ef1456acd934f8a
root <bradybod@gmail.com>
2019-12-10 23:30:13 +0000

    Permissions

--- plugins/ASIBanner/ASIBanner.php
+++ plugins/ASIBanner/ASIBanner.php
@@ -35,7 +35,7 @@ class ASIBannerPlugin extends TestlinkPlugin
 
   function title_bar()
   {
-    return("<div  style='width: 80%;'>" . file_get_contents(__DIR__.'/banner.html') . "</div>");
+    return("<div class='messages_rounded blocked' style='width: 80%;'>" . file_get_contents(__DIR__.'/banner.txt') . "</div>");
   }
 
 }
